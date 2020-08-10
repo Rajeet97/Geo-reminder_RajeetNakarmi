4ddb2dbeb22c73438ef93a2c8e231112b8130a4d
Rajet97 <rajeet37@gmail.com>
2020-08-10 20:35:31 +0545

    HelpActivity PermissionActivity added

---
+++ app/src/main/java/com/example/geo_reminder/activities/HelpActivity.java
@@ -0,0 +1,21 @@
+package com.example.geo_reminder.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.georeminder.R;
+
+public class HelpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+    }
+
+    public void goBack(View view) {
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/example/geo_reminder/activities/MapActivity.java
@@ -0,0 +1,23 @@
+package com.example.geo_reminder.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.georeminder.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+    }
+//for open new activity
+    public void btnClick(View view) {
+        startActivity(new Intent(MapActivity.this,DashboardActivity.class));
+    }
+}
