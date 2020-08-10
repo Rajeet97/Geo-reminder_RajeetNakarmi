8c16b5af165e3158499dfd1faedfb93919c28314
Rajet97 <rajeet37@gmail.com>
2020-08-10 08:34:30 +0545

    splash and AboutActivity added

---
+++ app/src/main/java/com/example/geo_reminder/activities/AboutActivity.java
@@ -0,0 +1,25 @@
+package com.example.geo_reminder.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.georeminder.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+    //Back click function
+    public void goBack(View view) {
+        super.onBackPressed();
+    }
+
+}
+
+
