5f4a98c564169df7640da5d5f770913dab38a36e
Rajet97 <rajeet37@gmail.com>
2020-08-15 00:01:14 +0545

    Junit testing

---
+++ app/src/test/java/com/example/geo_reminder/LongLatTest.java
@@ -0,0 +1,60 @@
+package com.example.geo_reminder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LongLatTest {
+
+    @Test
+    void get_id() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void set_id() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void getName() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void setName() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void getTask() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void setTask() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void getLon() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    void setLon() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void getLat() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void setLat() {
+        assertEquals(4, 2 + 2);
+    }
+}
